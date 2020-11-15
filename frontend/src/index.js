c7d4b3f72c2ab0abe5fc62b4d058a5d4b1f3c0bf
Yusef <yusefm8@gmail.com>
2020-11-15 13:30:51 -0500

    Setup initial states and UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,30 +5,67 @@ import axios from 'axios';
 import config from '../config';
 import logo from './static/logo.svg';
 import './style.css';
+import { Button, Dropdown } from 'semantic-ui-react';
+
+// Define dropdown options
+const dropdownOptions = [
+  {
+    key: "Username",
+    text: "Username",
+    value: "username",
+  },
+  {
+    key: "Name",
+    text: "Name",
+    value: "name",
+  },
+  {
+    key: "Category",
+    text: "Category",
+    value: "category",
+  },
+]
 
 /*
 This is a stubbed React functional component using React Effects and Hooks
 https://reactjs.org/docs/hooks-effect.html
 Feel free to use React class components and lifecycle hooks if that is more comfortable for you
 */
+
 function App() {
-  const [dataFromApi, setDataFromApi] = useState();
 
-  useEffect(() => {
-    async function fetchDataFromApi() {
-      const apiResponse = await axios.get(config.API_URL);
-      setDataFromApi(apiResponse.data);
-    }
-    fetchDataFromApi();
-  }, []);
+  // Define state
+  const [data, setData] = useState([]);
+  const [keyword, setKeyWord] = useState();
+  const [dropdown, setDropdown] = useState();
 
+  const searchBoxStyle = { width: "20rem", background: "#F2F1F9", borderRadius: "5px", border: "none", padding: "1rem", marginLeft: "1.5rem", marginRight: "1.5rem", marginTop: "3rem" };
   return (
     <div id="app">
       <img src={logo} alt="logo" />
-      <h1>Welcome to your interview!</h1>
-      {dataFromApi}
-    </div>
+      <form>
+        <Dropdown
+          placeholder='Search by'
+          selection
+          options={dropdownOptions}
+          onChange={(e, data) => setDropdown(data.value)}
+        />
+        <input style={searchBoxStyle}
+          type="text"
+          placeholder="Search for Cameo talent"
+          onChange={(e) => setKeyWord(e.target.value)}
+        />
+        <Button>
+          Submit
+        </Button>
+      </form>
+    </div >
   );
 }
+
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 ReactDOM.render(<App />, document.getElementById('main'));
